c0ff6d2f31e3d781669e56ae6ad91366508a3f0f
Nivedita AP <niveditaprasannaraj@gmail.com>
2022-08-31 13:18:07 +0530

    update: handled negative values

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ function calcuateSumOfAngles(one, two, three){
 }
 
 function checkTriangle(){
-    const sumOfAngles = calcuateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-    if(sumOfAngles === 180){
-        outputTriangle.innerText = "Yay! It forms a triangle.";
+
+    if(inputs[0].value>0  && inputs[1].value>0 && inputs[2].value > 0){
+        const sumOfAngles = calcuateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+        if(sumOfAngles === 180){
+            outputTriangle.innerText = "Yay! It forms a triangle.";
+        }
+        else{
+            outputTriangle.innerText = "Nope, angles does not form triangle.";
+        }
+    }else{
+        outputTriangle.innerText = "The angles should be greater than 0";
     }
-    else{
-        outputTriangle.innerText = "Nope, angles does not form triangle."
-    }
+    
 }
 
 isTriangleBtn.addEventListener("click", checkTriangle);
